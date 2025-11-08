778c8c8fcf59338bc54fb2f220c0f148ce329e78
Jhonconnors <anonimat.jk@gmail.com>
2025-11-08 16:53:42 -0300

    first commit

---
+++ src/main/java/com/example/MicroserviceSecondApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSecondApplication.class, args);
+	}
+
+}
